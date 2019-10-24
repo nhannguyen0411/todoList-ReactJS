02c3bedecbce57a5b3c62b19239b8d2651fc8627
Nhan <nguyentrongnhanc8@gmail.com>
2019-10-25 01:40:18 +0700

    Create todoItem and pass parameter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import TodoItem from './components/todoItem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.todoItems = [
+      'No way',
+      'Come on now'
+    ];
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <TodoItem title="No way"/>
+        <TodoItem title="Come on now"/>
+      </div>
+    );
+  }
 }
 
 export default App;
